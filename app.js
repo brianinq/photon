c4f3b4de59b17e5ce5a73ef591a34f4a34ddf3bf
brianinq <tayloroovse@gmail.com>
2021-05-08 16:55:43 +0300

    implement load more functionality

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const gallery = document.querySelector('.pics')
 const searchInput = document.querySelector('.search')
 const form = document.querySelector('.search-form')
 let searchValue;
-
-
+const more = document.querySelector('.more')
+let page = 1;
+let fetchLink;
+let currentSearch;
 //
 searchInput.addEventListener('input', updateInput)
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     searchPics(searchValue);
+    currentSearch = searchValue;
 })
+more.addEventListener('click', loadMore)
 
 function updateInput(event) {
     searchValue = event.target.value;
@@ -42,17 +46,18 @@ function generatePics(data) {
 
 
 async function curatedPics() {
-    const data = await fetchApi('https://api.pexels.com/v1/curated?per_page=&page=1')
+    fetchLink = 'https://api.pexels.com/v1/curated?per_page=&page=1'
+    const data = await fetchApi(fetchLink)
 
     generatePics(data)
 }
 
 async function searchPics(query) {
     clear()
-    const data = await fetchApi(`https://api.pexels.com/v1/search/?page=1&per_page=&query=${query}`)
+    fetchLink = `https://api.pexels.com/v1/search/?page=1&per_page=&query=${query}`
+    const data = await fetchApi(fetchLink)
 
     generatePics(data)
-    console.log(data)
 }
 
 function clear() {
@@ -60,4 +65,16 @@ function clear() {
     searchInput.value = '';
 }
 
+async function loadMore() {
+    page++;
+    if (currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search/?page=${page}&per_page=&query=${currentSearch}`
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=&page=${page}`
+    }
+
+
+    const data = await fetchApi(fetchLink)
+    generatePics(data)
+}
 curatedPics()
